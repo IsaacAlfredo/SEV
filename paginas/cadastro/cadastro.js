7031a5fc6d14968a82be024b0945078553e479a0
Isaac Alfredo <isaacfreitas23@gmail.com>
2022-06-04 18:22:03 -0300

    just some corrections

--- paginas/cadastro/cadastro.js
+++ paginas/cadastro/cadastro.js
@@ -3,6 +3,11 @@ import Bd from "../../Bd.js";
 var alunos = new Bd();
 var professores = new Bd();
 
-function cadastrar() {}
+function cadastrar() {
+
+    let nome = document.querySelector("input#nome").value
+    window.alert(nome)
+
+}
 
 export { alunos, professores };
